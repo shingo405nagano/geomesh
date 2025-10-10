bcbdc43782f3ab654f05652dd29809ed4ea7f774
shingo405nagano <makishin.nagano@gmail.com>
2025-10-10 04:17:14 +0000

    [Update]

--- geomesh/data.py
+++ geomesh/data.py
@@ -2,7 +2,6 @@
 Dataクラスの定義
 """
 
-from dataclasses import dataclass
 from decimal import Decimal
 from typing import NamedTuple
 
@@ -41,27 +40,3 @@ class Bounds(NamedTuple):
     y_min: float
     x_max: float
     y_max: float
-
-
-@dataclass
-class MeshDesignJP(object):
-    """
-    ## Summary:
-        日本で使用される、地域メッシュを作成する為のクラス
-    Args:
-        lon(float):
-            経度（10進数）
-        lat(float):
-            緯度（10進数）
-    """
-
-    lon: float
-    lat: float
-
-    def __post_init__(self):
-        if not (-180.0 <= self.lon <= 180.0):
-            raise ValueError("経度は-180.0から180.0の範囲で指定してください。")
-        if not (-90.0 <= self.lat <= 90.0):
-            raise ValueError("緯度は-90.0から90.0の範囲で指定してください。")
-        self.lon = float(self.lon)
-        self.lat = float(self.lat)
